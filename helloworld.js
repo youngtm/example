80cc8e68e7f6ded835f7a46b0421afb31bdf9860
Thomas Young <32685697+youngtm@users.noreply.github.com>
2021-03-03 15:08:48 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('Hello World');
+  res.end('Where Kubernetes is a pleasure!');
 });
 server.listen(8080);
